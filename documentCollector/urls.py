5e14560875ccb75196101681fc4e3671061432d5
Tanmoy Sarkar <ts741127@gmail.com>
2022-09-30 21:55:06 +0530

    update

--- documentCollector/urls.py
+++ documentCollector/urls.py
@@ -4,10 +4,12 @@ from django.urls import path, include
 
 from documentCollector import settings
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("studentportal.urls"))
 ]
 
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
